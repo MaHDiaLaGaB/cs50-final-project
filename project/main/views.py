ff206690d1393e5aaaf62bad53ffd29ec9b86281
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-01-23 16:01:01 +0300

    created auth and validation, everything is workin

--- project/main/views.py
+++ project/main/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, Response
 from flask_login import login_required
+from .live import LiveStreaming
 
 
 main = Blueprint('main', __name__)
@@ -10,8 +11,18 @@ def index():
     return render_template('index.html')
 
 
-
-@main.route('/live',methods=['GET'])
+@main.route('/live', methods=['GET'])
 @login_required
 def live():
     return render_template('live.html')
+
+
+# def stream(camera):
+#     while True:
+#         frame = camera.gen_frames()
+#         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+# @main.route('/video_feed')
+# def video_feed():
+#     return Response(stream(LiveStreaming()), mimetype='multipart/x-mixed-replace; boundary=frame')
